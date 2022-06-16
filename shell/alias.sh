92c5508730149ece0a1de7561b46910f932661e9
Hector Duran <dany0997@gmail.com>
2022-06-16 00:45:27 -0500

    add shell aliases

---
+++ shell/alias.sh
@@ -0,0 +1,22 @@
+# Aliases for common commands by hector97i
+# Git
+alias gs="git status"
+alias gf="git fetch"
+alias glog="git log --oneline --graph"
+alias glo="git log --oneline"
+alias gp="git push"
+alias gc="git commit"
+alias gpf="git push --force"
+alias gpl="git pull"
+alias ga="git add"
+alias gaa="git add ."
+# Django
+alias pmr="python manage.py runserver"
+alias pmmm="python manage.py makemigrations"
+alias pmm="python manage.py migrate"
+alias pmsh="python manage.py shell"
+alias pmsm="python manage.py showmigrations"
+# Python
+alias py="python"
+alias py3="python3"
+alias pirr="pip install -r requirements.txt"
---
+++ shell/install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo "Appending aliases..."
+cat ./alias.sh >> ~/.bashrc
+echo "Done :tada:"
